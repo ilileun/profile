bc551b2a047fd70b5ecdcef27f7b640716cba4b2
최지은 <jichoi@choejieun-ui-MacBookPro.local>
2026-03-31 03:19:53 +0900

    Remove News section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Navigation from "@/components/Navigation";
 import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
 import SkillsSection from "@/components/SkillsSection";
 import ExperienceSection from "@/components/ExperienceSection";
 import ProjectsSection from "@/components/ProjectsSection";
@@ -14,7 +13,6 @@ export default function Home() {
     <main>
       <Navigation />
       <HeroSection />
-      <AboutSection />
       <SkillsSection />
       <ExperienceSection />
       <ProjectsSection />
